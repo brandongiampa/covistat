bffc195a7c9d5ce0560fef53e2a6f84d468a991a
Brandon Giampa <30915668+brandongiampa@users.noreply.github.com>
2023-03-31 22:28:59 -0700

    Add disclaimer to bar graph in case of null input, make unrequired to prevent console error.

--- src/components/reusable/BarGraph.jsx
+++ src/components/reusable/BarGraph.jsx
@@ -23,41 +23,58 @@ ChartJS.register(
 
 function BarGraph( { title, countryStat, worldStat } ) {
 
-  return (
-    <div className="flex-1 container p-6 mx-auto">
-        <h3 className="text-center text-xl lg:text-4xl weight-500 mb-3 underline">{ title }</h3>
-        <Bar 
-            data={
+    if (countryStat !== null || worldStat !== null) {
+        return (
+            <div className="flex-1 container p-6 mx-auto">
+                <h3 className="text-center text-xl lg:text-4xl weight-500 mb-3 underline">{ title }</h3>
                 {
-                    labels: [`${ title }/million`],
-                    datasets: [
-                        {
-                            label: 'Country',
-                            data: [countryStat],
-                            borderWidth: 1,
-                            backgroundColor: '#1AE6CC'
-                        },
-                        {
-                            label: 'World',
-                            data: [worldStat],
-                            borderWidth: 1,
-                            backgroundColor: 'rgb(102,26,230)'
-                        }
-                    ],
-                    options: {
-                        responsive: true
-                    }
+                    !countryStat && (
+                        <p className="text-danger text-center mb-1">*Data is not set for the '{title}' category in this country at this moment.</p>
+                    )
                 }
-            }
-        />
-    </div>
-  )
+                {
+                    !worldStat && (
+                        <p className="text-danger text-center mb-1">*Data is not set for the '{title}' category for all countries at this moment.</p>
+                    )
+                }
+                <Bar 
+                    data={
+                        {
+                            labels: [`${ title }/million`],
+                            datasets: [
+                                {
+                                    label: 'Country',
+                                    data: [countryStat],
+                                    borderWidth: 1,
+                                    backgroundColor: '#1AE6CC'
+                                },
+                                {
+                                    label: 'World',
+                                    data: [worldStat],
+                                    borderWidth: 1,
+                                    backgroundColor: 'rgb(102,26,230)'
+                                }
+                            ],
+                            options: {
+                                responsive: true
+                            }
+                        }
+                    }
+                />
+            </div>
+        )
+    }
+    else {
+        return (
+            <></>
+        )
+    }
 }
 
 BarGraph.propTypes = {
     title: PropTypes.string.isRequired,
-    countryStat: PropTypes.number.isRequired,
-    worldStat: PropTypes.number.isRequired,
+    countryStat: PropTypes.number,
+    worldStat: PropTypes.number,
 }
 
 export default BarGraph
